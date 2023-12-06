b0a05eed69cedf2c33402f91e0138105dbaaf6a9
Diogenes Lemos <diogenes.lemos1@gmail.com>
2023-12-06 15:06:26 -0300

    versão inicial

---
+++ main.js
@@ -0,0 +1,20 @@
+function validationMachine() {
+    let campoAvalor = document.getElementById("campoA").value;
+    let campoBvalor = document.getElementById("campoB").value;
+
+    if (campoBvalor <= campoAvalor) {
+        alert("O valor de B não pode ser igual ou menor do que o de A, favor corrigir!");
+        document.getElementById("standart").style.display = "none";
+        document.getElementById("error").style.display = "block";
+        return false;
+    } else {
+        alert("Tudo certo!, submetido com sucesso");
+        return true;
+    }
+}
+
+document.getElementById("campos").addEventListener("submit", function(event) {
+    if (!validationMachine()) {
+        event.preventDefault();
+    }
+});
